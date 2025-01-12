bac36d0e1a49b6b565c8e483cdb2daf39c3bfc02
Jun Masui <76625904+junmasui@users.noreply.github.com>
2025-01-12 07:47:00 -0500

    use password generator

--- quick_fill_secrets.sh
+++ quick_fill_secrets.sh
@@ -4,17 +4,17 @@ export $(grep -v '^#' quick_fill.secrets.env | xargs -d '\n')
 
 # Auto-populate passwords that will never leave the local Docker environment.
 if [ -z "$MINIO_ROOT_PASSWORD" ]; then
-    export MINIO_ROOT_PASSWORD=miniopass_123
+    export MINIO_ROOT_PASSWORD=minio_$(gpg --gen-random --armor 1 16)
 fi
 if [ -z "$BACKEND_MINIO_USER_PASSWORD" ]; then
-    export BACKEND_MINIO_USER_PASSWORD=backendpass_123
+    export BACKEND_MINIO_USER_PASSWORD=backend_minio_$(gpg --gen-random --armor 1 16)
 fi
 
 if [ -z "$POSTGRES_PASSWORD" ]; then
-    export POSTGRES_PASSWORD=postgrespass
+    export POSTGRES_PASSWORD="postgres_$(gpg --gen-random --armor 1 16)"
 fi
 if [ -z "$BACKEND_POSTGRES_USER_PASSWORD" ]; then
-    export BACKEND_POSTGRES_USER_PASSWORD=langchainpass
+    export BACKEND_POSTGRES_USER_PASSWORD="backend_postgres_$(gpg --gen-random --armor 1 16)"
 fi
 
 # Generate the services' .secrets.env
