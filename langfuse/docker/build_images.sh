5e6a91494902a7811d753dfb6b831dfb92b88c4d
Jun Masui <76625904+junmasui@users.noreply.github.com>
2025-02-07 00:20:27 -0500

    got end-to-end to succeed with tracing

--- langfuse/docker/build_images.sh
+++ langfuse/docker/build_images.sh
@@ -4,3 +4,10 @@ docker buildx build \
   --progress=plain \
   --tag localhost/localdomain-langfuse:3.24 \
   .
+
+docker buildx build \
+  --build-context parent-dir=.. \
+  --file worker.Dockerfile \
+  --progress=plain \
+  --tag localhost/localdomain-langfuse-worker:3.24 \
+  .
--- quick_fill_secrets.sh
+++ quick_fill_secrets.sh
@@ -6,94 +6,97 @@ export $(grep -v '^#' quick_fill.secrets.env | xargs -d '\n')
 
 if [ -z "$APPLICATION_JWT_SECRET" ]; then
     export APPLICATION_JWT_SECRET="$(openssl rand -hex 32)"
-    echo -e "\n# (Auto-generated)  APPLICATION_JWT_SECRET is created thur openssl rand -hex 32" >> quick_fill.secrets.env
+    echo -e "\n# APPLICATION_JWT_SECRET is created thru openssl rand -hex 32.  (auto-generated secret)" >> quick_fill.secrets.env
     echo "APPLICATION_JWT_SECRET=${APPLICATION_JWT_SECRET}" >> quick_fill.secrets.env
 fi
 
 if [ -z "$MINIO_ROOT_PASSWORD" ]; then
-    export MINIO_ROOT_PASSWORD=minio_$(gpg --gen-random --armor 1 16)
-    echo -e "\n# (Auto-generated)  Minio's root account's password" >> quick_fill.secrets.env
+    export MINIO_ROOT_PASSWORD=minio_$(gpg --gen-random --armour 1 16 | tr '+/' '-_' | tr -d '=')
+    echo -e "\n# Minio's root account's password.  (auto-generated secret)" >> quick_fill.secrets.env
     echo "MINIO_ROOT_PASSWORD=${MINIO_ROOT_PASSWORD}" >> quick_fill.secrets.env
 fi
 if [ -z "$BACKEND_MINIO_USER_PASSWORD" ]; then
-    export BACKEND_MINIO_USER_PASSWORD=backend_minio_$(gpg --gen-random --armor 1 16)
-    echo -e "\n# (Auto-generated)  backend's Minio\n# account's password." >> quick_fill.secrets.env
+    export BACKEND_MINIO_USER_PASSWORD=backend_minio_$(gpg --gen-random --armour 1 16 | tr '+/' '-_' | tr -d '=')
+    echo -e "\n# backend's Minio\n# account's password.  (auto-generated secret)" >> quick_fill.secrets.env
     echo "BACKEND_MINIO_USER_PASSWORD=${BACKEND_MINIO_USER_PASSWORD}" >> quick_fill.secrets.env
 fi
 if [ -z "$LANGFUSE_MINIO_USER_PASSWORD" ]; then
-    export LANGFUSE_MINIO_USER_PASSWORD=langfuse_minio_$(gpg --gen-random --armor 1 16)
-    echo -e "\n# (Auto-generated)  Langfuse's Minio account's password." >> quick_fill.secrets.env
+    export LANGFUSE_MINIO_USER_PASSWORD=langfuse_minio_$(gpg --gen-random --armour 1 16 | tr '+/' '-_' | tr -d '=')
+    echo -e "\n# Langfuse's Minio account's password.  (auto-generated secret)" >> quick_fill.secrets.env
     echo "LANGFUSE_MINIO_USER_PASSWORD=${LANGFUSE_MINIO_USER_PASSWORD}" >> quick_fill.secrets.env
 fi
-export LANGFUSE_MINIO_USER_PASSWORD_ENCODED=$( echo -n $LANGFUSE_MINIO_USER_PASSWORD | jq -sRr '@uri' )
+
+if [ -z "$REDIS_DEFAULT_PASSWORD" ]; then
+    export REDIS_DEFAULT_PASSWORD="redis_$(gpg --gen-random --armour 1 16 | tr '+/' '-_' | tr -d '=')"
+    echo -e "\n# REDIS_DEFAULT_PASSWORD contains Redis's default account's password  (auto-generated secret)" >> quick_fill.secrets.env
+    echo "REDIS_DEFAULT_PASSWORD=${REDIS_DEFAULT_PASSWORD}" >> quick_fill.secrets.env
+fi
 
 
 if [ -z "$POSTGRES_PASSWORD" ]; then
-    export POSTGRES_PASSWORD="postgres_$(gpg --gen-random --armor 1 16)"
-    echo -e "\n# (Auto-generated)  POSTGRES_PASSWORD contains Postgres's root account's password" >> quick_fill.secrets.env
+    export POSTGRES_PASSWORD="postgres_$(gpg --gen-random --armour 1 16 | tr '+/' '-_' | tr -d '=')"
+    echo -e "\n# POSTGRES_PASSWORD contains Postgres's root account's password  (auto-generated secret)" >> quick_fill.secrets.env
     echo "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}" >> quick_fill.secrets.env
 fi
 if [ -z "$BACKEND_POSTGRES_USER_PASSWORD" ]; then
-    export BACKEND_POSTGRES_USER_PASSWORD="backend_postgres_$(gpg --gen-random --armor 1 16)"
-    echo -e "\n# (Auto-generated)  backend's Postgres account's password." >> quick_fill.secrets.env
+    export BACKEND_POSTGRES_USER_PASSWORD="backend_postgres_$(gpg --gen-random --armour 1 16 | tr '+/' '-_' | tr -d '=')"
+    echo -e "\n# backend's Postgres account's password.  (auto-generated secret)" >> quick_fill.secrets.env
     echo "BACKEND_POSTGRES_USER_PASSWORD=${BACKEND_POSTGRES_USER_PASSWORD}" >> quick_fill.secrets.env
 fi
 if [ -z "$LANGFUSE_POSTGRES_USER_PASSWORD" ]; then
-    export LANGFUSE_POSTGRES_USER_PASSWORD="langfuse_postgres_$(gpg --gen-random --armor 1 16)"
-    echo -e "\n# (Auto-generated)  Langfuse's Postgres account's password." >> quick_fill.secrets.env
+    export LANGFUSE_POSTGRES_USER_PASSWORD="langfuse_postgres_$(gpg --gen-random --armour 1 16 | tr '+/' '-_' | tr -d '=')"
+    echo -e "\n# Langfuse's Postgres account's password.  (auto-generated secret)" >> quick_fill.secrets.env
     echo "LANGFUSE_POSTGRES_USER_PASSWORD=${LANGFUSE_POSTGRES_USER_PASSWORD}" >> quick_fill.secrets.env
 fi
-export LANGFUSE_POSTGRES_USER_PASSWORD_ENCODED=$( echo -n $LANGFUSE_POSTGRES_USER_PASSWORD | jq -sRr '@uri' )
 
 if [ -z "$GRAFANA_ADMIN_PASSWORD" ]; then
-    export GRAFANA_ADMIN_PASSWORD=grafana_$(gpg --gen-random --armor 1 16)
-    echo -e "\n# (Auto-generated)  Grafrana's admin account's password" >> quick_fill.secrets.env
+    export GRAFANA_ADMIN_PASSWORD=grafana_$(gpg --gen-random --armour 1 16 | tr '+/' '-_' | tr -d '=')
+    echo -e "\n# Grafrana's admin account's password  (auto-generated secret)" >> quick_fill.secrets.env
     echo "GRAFANA_ADMIN_PASSWORD=${GRAFANA_ADMIN_PASSWORD}" >> quick_fill.secrets.env
 fi
 
 if [ -z "$CLICKHOUSE_DEFAULT_USER_PASSWORD" ]; then
-    export CLICKHOUSE_DEFAULT_USER_PASSWORD=clickhouse_default_$(gpg --gen-random --armor 1 16)
-    echo -e "\n# (Auto-generated)  Clickhouse's default account's password" >> quick_fill.secrets.env
+    export CLICKHOUSE_DEFAULT_USER_PASSWORD=clickhouse_default_$(gpg --gen-random --armour 1 16 | tr '+/' '-_' | tr -d '=')
+    echo -e "\n# Clickhouse's default account's password  (auto-generated secret)" >> quick_fill.secrets.env
     echo "CLICKHOUSE_DEFAULT_USER_PASSWORD=${CLICKHOUSE_DEFAULT_USER_PASSWORD}" >> quick_fill.secrets.env
 fi
 
 if [ -z "$CLICKHOUSE_ADMIN_USER_PASSWORD" ]; then
-    export CLICKHOUSE_ADMIN_USER_PASSWORD=clickhouse_admin_$(gpg --gen-random --armor 1 16)
-    echo -e "\n# (Auto-generated)  Clickhouse's admin account's password" >> quick_fill.secrets.env
+    export CLICKHOUSE_ADMIN_USER_PASSWORD=clickhouse_admin_$(gpg --gen-random --armour 1 16 | tr '+/' '-_' | tr -d '=')
+    echo -e "\n# Clickhouse's admin account's password  (auto-generated secret)" >> quick_fill.secrets.env
     echo "CLICKHOUSE_ADMIN_USER_PASSWORD=${CLICKHOUSE_ADMIN_USER_PASSWORD}" >> quick_fill.secrets.env
 fi
 
 if [ -z "$LANGFUSE_CLICKHOUSE_USER_PASSWORD" ]; then
-    export LANGFUSE_CLICKHOUSE_USER_PASSWORD=backend_clickhouse_$(gpg --gen-random --armor 1 16)
-    echo -e "\n# (Auto-generated)  backend's Clickhouse\n# account's password." >> quick_fill.secrets.env
+    export LANGFUSE_CLICKHOUSE_USER_PASSWORD=langfuse_clickhouse_$(gpg --gen-random --armour 1 16| tr '+/' '-_' | tr -d '=')
+    echo -e "\n# backend's Clickhouse account's password.  (auto-generated secret)" >> quick_fill.secrets.env
     echo "LANGFUSE_CLICKHOUSE_USER_PASSWORD=${LANGFUSE_CLICKHOUSE_USER_PASSWORD}" >> quick_fill.secrets.env
 fi
-export LANGFUSE_CLICKHOUSE_USER_PASSWORD_ENCODED=$( echo -n $LANGFUSE_CLICKHOUSE_USER_PASSWORD | jq -sRr '@uri' )
 
 if [ -z "$LANGFUSE_SALT" ]; then
-    export LANGFUSE_SALT=$(openssl rand -base64 32)
-    echo -e "\n# (Auto-generated)  LANGFUSE_SALT contains langfuse's salt." >> quick_fill.secrets.env
+    export LANGFUSE_SALT=$(openssl rand -base64 32 | tr '+/' '-_' | tr -d '=')
+    echo -e "\n# LANGFUSE_SALT contains langfuse's salt.  (auto-generated secret)" >> quick_fill.secrets.env
     echo "LANGFUSE_SALT=${LANGFUSE_SALT}" >> quick_fill.secrets.env
 fi
 if [ -z "$LANGFUSE_ENCRYPTION_KEY" ]; then
     export LANGFUSE_ENCRYPTION_KEY=$(openssl rand -hex 32)
-    echo -e "\n# (Auto-generated)  LANGFUSE_ENCRYPTION_KEY contains langfuse's encryption key." >> quick_fill.secrets.env
+    echo -e "\n# LANGFUSE_ENCRYPTION_KEY contains langfuse's encryption key.  (auto-generated secret)" >> quick_fill.secrets.env
     echo "LANGFUSE_ENCRYPTION_KEY=${LANGFUSE_ENCRYPTION_KEY}" >> quick_fill.secrets.env
 fi
 
 if [ -z "$LANGFUSE_INIT_USER_PASSWORD" ]; then
     export LANGFUSE_INIT_USER_PASSWORD=lf_pw_$(openssl rand -hex 8)
-    echo -e "\n# (Auto-generated)  langfuse's initial users's password." >> quick_fill.secrets.env
+    echo -e "\n# langfuse's initial users's password.  (auto-generated secret)" >> quick_fill.secrets.env
     echo "LANGFUSE_INIT_USER_PASSWORD=${LANGFUSE_INIT_USER_PASSWORD}" >> quick_fill.secrets.env
 fi
 if [ -z "$LANGFUSE_INIT_PROJECT_SECRET_KEY" ]; then
     export LANGFUSE_INIT_PROJECT_SECRET_KEY=sk-lf-$( uuidgen )
-    echo -e "\n# (Auto-generated)  langfuse's initial project's secret key." >> quick_fill.secrets.env
+    echo -e "\n# langfuse's initial project's secret key.  (auto-generated secret)" >> quick_fill.secrets.env
     echo "LANGFUSE_INIT_PROJECT_SECRET_KEY=${LANGFUSE_INIT_PROJECT_SECRET_KEY}" >> quick_fill.secrets.env
 fi
 if [ -z "$LANGFUSE_INIT_PROJECT_PUBLIC_KEY" ]; then
     export LANGFUSE_INIT_PROJECT_PUBLIC_KEY=pk-lf-$( uuidgen )
-    echo -e "\n# # (Auto-generated)  langfuse's initial project's public key." >> quick_fill.secrets.env
+    echo -e "\n# # langfuse's initial project's public key.  (auto-generated secret)" >> quick_fill.secrets.env
     echo "LANGFUSE_INIT_PROJECT_PUBLIC_KEY=${LANGFUSE_INIT_PROJECT_PUBLIC_KEY}" >> quick_fill.secrets.env
 fi
 
@@ -109,7 +112,8 @@ for RELPATH in "backend/backend.secrets.env" \
                "minio/minio-init.secrets.env" \
                "minio/minio.secrets.env" \
                "postgres/pgvector-init.secrets.env" \
-               "postgres/pgvector.secrets.env"
+               "postgres/pgvector.secrets.env" \
+               "redis/redis.conf"
 do
     ( envsubst < ${RELPATH}.template > ${RELPATH} )
 done
