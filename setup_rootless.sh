77e68d845d397977d41d14936db4abbc83f7ba4b
junmasui <76625904+junmasui@users.noreply.github.com>
2025-03-05 21:31:11 -0500

    leave some scripts untouched (#36)

--- setup_rootless.sh
+++ setup_rootless.sh
@@ -26,36 +26,44 @@ do
     fi
 done
 
-for RELPATH in "frontend/" "backend/"
+for RELPATH in "frontend" "backend"
 do
     sudo find $RELPATH -type d \
+        -not \( -path "${RELPATH}/docker" \) \
         -exec chown rootless-1000:rootless-1000 \{\} \; \
         -exec chmod ug=srwx,o=rx \{\} \;
     sudo find $RELPATH -type f \
+        -not \( -path "${RELPATH}/*.sh" -or -path "${RELPATH}/docker/*" \) \
         -exec chown rootless-1000:rootless-1000 \{\} \; \
         -exec chmod ug=rw,o=r \{\}  \;
+    sudo find $RELPATH -type f \
+        \( -path "${RELPATH}/run*.sh" \) \
+        -exec chown rootless-1000:rootless-1000 \{\} \; \
+        -exec chmod ug=rwx,o=r \{\}  \;
 done
 
 
-for RELPATH in "clickhouse/"
+for RELPATH in "clickhouse"
 do
-    sudo find clickhouse/ -iname '*.xml' -type f \
+    sudo find $RELPATH -iname '*.xml' -type f \
         -exec chown rootless-101:rootless-101 \{\} \; \
         -exec chmod ug=rw \{\}  \;
 done
 
-sudo find postgres/ -iname '*.sql.template' -type f \
-    -exec chown rootless-999:rootless-999 \{\} \; \
-    -exec chmod ug=rw,o=r \{\}  \;
+for RELPATH in "postgres"
+do
+    sudo find $RELPATH -iname '*.sql.template' -type f \
+        -not \( -path "${RELPATH}/docker/*" \) \
+        -exec chown rootless-999:rootless-999 \{\} \; \
+        -exec chmod ug=rw,o=r \{\}  \;
 
-sudo find postgres/ -iname '*.sh' -type f \
-    -exec chown rootless-999:rootless-999 \{\} \; \
-    -exec chmod ug=rw,o=r \{\}  \;
+    sudo find $RELPATH -iname '*.sh' -type f \
+        -not \( -path "${RELPATH}/docker/*" \) \
+        -exec chown rootless-999:rootless-999 \{\} \; \
+        -exec chmod ug=rw,o=r \{\}  \;
+done
 
-sudo find redis/ -iname '*.conf' -type f \
+sudo find redis -iname '*.conf' -type f \
     -exec chown rootless-999:rootless-999 \{\} \; \
     -exec chmod ug=rw \{\}  \;
 
-sudo find clickhouse/ -iname '*admin-user.xml' -type f \
-    -exec chown rootless-101:rootless-101 \{\} \; \
-    -exec chmod ug=rw \{\}  \;
