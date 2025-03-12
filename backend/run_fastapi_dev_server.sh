d289cca7e6e4faf8c04b151d7f11caa43e31350e
junmasui <76625904+junmasui@users.noreply.github.com>
2025-03-11 20:46:02 -0400

    Support in-container debugging (#42)

--- backend/run_fastapi_dev_server.sh
+++ backend/run_fastapi_dev_server.sh
@@ -11,8 +11,20 @@ else
     exit -1
 fi
 
+PYTHON_CMD_OPTIONS="-m uvicorn core_app:app --host 0.0.0.0 --port 8100"
 
+PYTHON_CMD_OPTIONS="-m debugpy --listen 0.0.0.0:5678 ${PYTHON_CMD_OPTIONS}"
+
+# Run the FastAPI server with debugpy.
+
+PYTHON_CMD="python3 ${PYTHON_CMD_OPTIONS}"
+
+# Use watchmedo to restart on file changes.
+WATCHMEDO_OPTIONS="auto-restart --directory=.  --recursive --pattern='*.py;*.env'"
+PYTHON_CMD="watchmedo ${WATCHMEDO_OPTIONS} -- ${PYTHON_CMD}"
+
+echo "${PYTHON_CMD}"
+
+# Use 'uv run' to run the fastapi server.
 PYTHONPATH=./src \
-uv run --frozen --no-sync -- watchmedo auto-restart \
-   --directory=.  --recursive --pattern='*.py;*.env' \
-   -- uvicorn core_app:app --host 0.0.0.0 --port 8100
+    uv run --frozen --no-sync -- ${PYTHON_CMD}
