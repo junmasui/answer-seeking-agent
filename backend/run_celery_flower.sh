c260f89f31d1f7235d0055313c7fac1520a77342
junmasui <76625904+junmasui@users.noreply.github.com>
2025-04-29 11:45:15 -0400

    debounce the autostart (#60)

--- backend/run_celery_flower.sh
+++ backend/run_celery_flower.sh
@@ -42,10 +42,30 @@ export POSTGRES_CHECKPOINTS_CONNECTION_URL="postgresql+psycopg://${CHECKPOINTS_P
 export FLOWER_BASIC_AUTH=$CELERY_FLOWER_USER_NAME:$CELERY_FLOWER_USER_PASSWORD
 set -o history # turn it back on
 
+# NOTE 1 regarding watchmdo:
+#   There are two approaches to launching run-and-done executables from watchmedo:
+#     * shell-command
+#     * auto-restart with --no-restart-on-command-exit
+#   The major difference between the two is: shell-command does not try to kill the prior
+#   launchs, auto-restart does try to kill. For longer-running pytest commands, the prior
+#   launch should be killed since it is only consuming resources for an obsolete set of changes.
+#   Hence the auto-restart approach is choosen.
+#
+# NOTE 2 regarding watchmedo
+#   A deep dive regarding pattern option in watchmedo:
+#     You will need to start at
+#     AutoRestartTrick (https://github.com/gorakhargosh/watchdog/blob/561aa0425c44b9d4376163f2b909bf1b655cf71a/src/watchdog/tricks/__init__.py#L147)
+#     arriving at PatternMatchingEventHandler (https://github.com/gorakhargosh/watchdog/blob/561aa0425c44b9d4376163f2b909bf1b655cf71a/src/watchdog/events.py#L292)
+#     then arriving at _match_path (https://github.com/gorakhargosh/watchdog/blob/561aa0425c44b9d4376163f2b909bf1b655cf71a/src/watchdog/utils/patterns.py#L24)
+if [ -z "${WATCH_DEBOUNCE_SECS:-}" ]; then
+    WATCH_DEBOUNCE_SECS=5.0
+fi
+
 PYTHONPATH=./src \
 uv run --frozen --no-sync \
    -- \
    watchmedo auto-restart \
+   --debounce-interval=${WATCH_DEBOUNCE_SECS} \
    --directory=.  --recursive --pattern='*.py;*.env' \
    -- \
    celery --app=core_worker flower
--- backend/run_celery_worker.sh
+++ backend/run_celery_worker.sh
@@ -1,4 +1,3 @@
-
 set -e  # Exit immediately on error.
 set -u  # Unbound variables are errors.
 set -o pipefail  # Use right-most non-zero exit code from a pipe.
@@ -40,10 +39,30 @@ export POSTGRES_VECTORS_CONNECTION_URL="postgresql+psycopg://${VECTORS_POSTGRES_
 export POSTGRES_CHECKPOINTS_CONNECTION_URL="postgresql+psycopg://${CHECKPOINTS_POSTGRES_USER_NAME}:${CHECKPOINTS_POSTGRES_USER_PASSWORD}@pgvector:5432/${ANSWERS_POSTGRES_DATABASE}"
 set -o history # turn it back on
 
+# NOTE 1 regarding watchmdo:
+#   There are two approaches to launching run-and-done executables from watchmedo:
+#     * shell-command
+#     * auto-restart with --no-restart-on-command-exit
+#   The major difference between the two is: shell-command does not try to kill the prior
+#   launchs, auto-restart does try to kill. For longer-running pytest commands, the prior
+#   launch should be killed since it is only consuming resources for an obsolete set of changes.
+#   Hence the auto-restart approach is choosen.
+#
+# NOTE 2 regarding watchmedo
+#   A deep dive regarding pattern option in watchmedo:
+#     You will need to start at
+#     AutoRestartTrick (https://github.com/gorakhargosh/watchdog/blob/561aa0425c44b9d4376163f2b909bf1b655cf71a/src/watchdog/tricks/__init__.py#L147)
+#     arriving at PatternMatchingEventHandler (https://github.com/gorakhargosh/watchdog/blob/561aa0425c44b9d4376163f2b909bf1b655cf71a/src/watchdog/events.py#L292)
+#     then arriving at _match_path (https://github.com/gorakhargosh/watchdog/blob/561aa0425c44b9d4376163f2b909bf1b655cf71a/src/watchdog/utils/patterns.py#L24)
+if [ -z "${WATCH_DEBOUNCE_SECS:-}" ]; then
+    WATCH_DEBOUNCE_SECS=5.0
+fi
+
 PYTHONPATH=./src \
 uv run --frozen --no-sync \
    -- \
    watchmedo auto-restart \
+   --debounce-interval=${WATCH_DEBOUNCE_SECS} \
    --directory=./src  --recursive --pattern='*.py' \
    -- \
    celery --app=core_worker worker -l INFO
