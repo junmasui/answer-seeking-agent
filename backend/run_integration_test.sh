6b47fb5bb610a71c42724beb2a3530077ee77bcd
Jun Masui <76625904+junmasui@users.noreply.github.com>
2025-04-29 11:44:04 -0400

    debounce the auto-restart

--- backend/run_integration_test.sh
+++ backend/run_integration_test.sh
@@ -45,21 +45,31 @@ set -o history # turn it back on
 # fs.inotify.max_user_watches = 524288
 #
 
-# There are two approaches to launching run-and-done executables
-# from watchmedo:
-#  * shell-command
-#  * auto-restart with --no-restart-on-command-exit
-# The first approach is better suited for quick commands. shell-command does
-# not attempt to kill the prior launchs. For longer-running pytest commands,
-# the prior launch should be killed since it is only consuming resources
-# for an obsolete trigger. Hence the second approach is choosen.
+# NOTE 1 regarding watchmdo:
+#   There are two approaches to launching run-and-done executables from watchmedo:
+#     * shell-command
+#     * auto-restart with --no-restart-on-command-exit
+#   The major difference between the two is: shell-command does not try to kill the prior
+#   launchs, auto-restart does try to kill. For longer-running pytest commands, the prior
+#   launch should be killed since it is only consuming resources for an obsolete set of changes.
+#   Hence the auto-restart approach is choosen.
 #
+# NOTE 2 regarding watchmedo
+#   A deep dive regarding pattern option in watchmedo:
+#     You will need to start at
+#     AutoRestartTrick (https://github.com/gorakhargosh/watchdog/blob/561aa0425c44b9d4376163f2b909bf1b655cf71a/src/watchdog/tricks/__init__.py#L147)
+#     arriving at PatternMatchingEventHandler (https://github.com/gorakhargosh/watchdog/blob/561aa0425c44b9d4376163f2b909bf1b655cf71a/src/watchdog/events.py#L292)
+#     then arriving at _match_path (https://github.com/gorakhargosh/watchdog/blob/561aa0425c44b9d4376163f2b909bf1b655cf71a/src/watchdog/utils/patterns.py#L24)
+if [ -z "${WATCH_DEBOUNCE_SECS:-}" ]; then
+    WATCH_DEBOUNCE_SECS=5.0
+fi
+
 PYTHONPATH=./src:./tests \
 uv run --frozen --no-sync \
    -- \
    watchmedo auto-restart \
    --no-restart-on-command-exit \
-   --debounce-interval=5.0 \
+   --debounce-interval=${WATCH_DEBOUNCE_SECS} \
    --directory=./src --directory=./tests  --recursive --pattern='*.py' \
    -- \
    pytest -v -v --capture=tee-sys tests
