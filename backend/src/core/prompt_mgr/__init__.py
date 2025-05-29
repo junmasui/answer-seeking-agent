bedba9a81228bd17a45d0311910264db058bb6c6
Jun Masui <76625904+junmasui@users.noreply.github.com>
2025-05-29 09:41:57 -0400

    move functions to better locations

--- backend/src/core/prompt_mgr/__init__.py
+++ backend/src/core/prompt_mgr/__init__.py
@@ -1,38 +1,22 @@
-import textwrap
-
-from core.agent.internal_models import AgentPromptName
-from core.public_models import AgentPromptStatus
-
 from . import initial_prompts
-from .prompt import add_prompt, delete_prompt, get_prompt, get_prompt_statistics, list_prompts, update_prompt
+from .prompt import (
+    add_chat_prompt,
+    add_prompt,
+    delete_prompt,
+    get_prompt,
+    get_prompt_statistics,
+    list_prompts,
+    update_prompt,
+)
 
 # Explicitly define the exported names: these names are the contract of this module.
 __all__ = [
+    'add_chat_prompt',
     'add_prompt',
     'delete_prompt',
     'get_prompt',
     'get_prompt_statistics',
     'list_prompts',
     'update_prompt',
-    'add_chat_prompt',
     'initial_prompts',
 ]
-
-
-def add_chat_prompt(*, prompt_name: str, system_message: str = None, human_message: str = None):
-    """Add a prompt in the database if it does not exist, using the provided defaults."""
-    if prompt_name not in AgentPromptName:
-        raise ValueError('prompt_name must be a valid AgentPromptName constant')
-
-    result = list_prompts(name=prompt_name, status=AgentPromptStatus.ACTIVE)
-
-    if result.prompts:
-        return
-
-    if system_message:
-        system_message = textwrap.dedent(system_message)
-    if human_message:
-        human_message = textwrap.dedent(human_message)
-    add_prompt(
-        name=prompt_name, status=AgentPromptStatus.ACTIVE, human_message=human_message, system_message=system_message
-    )
