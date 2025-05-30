1387f49f04238a61d2e2ef5105646df9ee47f510
Jun Masui <76625904+junmasui@users.noreply.github.com>
2025-05-30 17:04:23 -0400

    reformat some comments

--- backend/src/core_worker/celeryconfig.py
+++ backend/src/core_worker/celeryconfig.py
@@ -16,7 +16,7 @@ _logger = _logging.getLogger(__name__)
 # See: https://docs.celeryq.dev/en/stable/userguide/configuration.html#redis-backend-settings
 #
 
-## The backend used to store task results (tombstones)
+# The backend used to store task results (tombstones)
 backend = str(_get_global_config().redis_dsn)
 
 #
@@ -25,7 +25,7 @@ backend = str(_get_global_config().redis_dsn)
 # See: https://docs.celeryq.dev/en/stable/userguide/configuration.html#broker-settings
 #
 
-## Default broker URL. Must be in the form: transport://userid:password@hostname:port/virtual_host
+# Default broker URL. Must be in the form: transport://userid:password@hostname:port/virtual_host
 broker_url = str(_get_global_config().redis_dsn)
 
 #
@@ -55,13 +55,13 @@ result_backend_transport_options = {
 # See https://docs.celeryq.dev/en/stable/userguide/configuration.html#worker
 #
 
-## The number of concurrent worker processes/threads/green threads executing tasks.
+# The number of concurrent worker processes/threads/green threads executing tasks.
 worker_concurrency = 2
 
-## Maximum number of tasks a pool worker process can execute before it’s replaced with a new one.
+# Maximum number of tasks a pool worker process can execute before it’s replaced with a new one.
 worker_max_tasks_per_child = 100
 
-## Maximum amount of resident memory, in kilobytes, that may be consumed by a worker before it will be replaced by a new worker.
+# Maximum amount of resident memory, in kilobytes, that may be consumed by a worker before it will be replaced by a new worker.
 # worker_max_memory_per_child=4_096_000 # 4 Gb
 worker_max_memory_per_child = 8_192_000  # 8 Gb
 
@@ -71,13 +71,13 @@ worker_max_memory_per_child = 8_192_000  # 8 Gb
 # See https://docs.celeryq.dev/en/stable/userguide/configuration.html#events
 #
 
-## Send task-related events so that tasks can be monitored
-##
-## True is required by the celery-exporter
+# Send task-related events so that tasks can be monitored
+#
+# True is required by the celery-exporter
 worker_send_task_events = True
 
-## When enabled, a task-sent event will be sent for every task so that
-## tasks can be tracked before they’re consumed by a worker.
-##
-## True is required by the celery-exporter
+# When enabled, a task-sent event will be sent for every task so that
+# tasks can be tracked before they’re consumed by a worker.
+#
+# True is required by the celery-exporter
 task_send_sent_event = True
