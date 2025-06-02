14a09773577a5312df591093986d55992067061a
Jun Masui <76625904+junmasui@users.noreply.github.com>
2025-06-02 18:16:18 -0400

    it works

---
+++ frontend/traefik/custom-docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+## set -e  # Exit immediately on error.
+set -u  # Unbound variables are errors.
+## set -o pipefail  # Use right-most non-zero exit code from a pipe.
+
+
+touch /acme/acme.json
+chmod 600 /acme/acme.json
+
+# Process with original entrypoint, which can be discovered
+# from the host command-line with:
+#   docker inspect traefik:v3.4 | jq '.[0].Config.Entrypoint'
+exec /entrypoint.sh "$@"
