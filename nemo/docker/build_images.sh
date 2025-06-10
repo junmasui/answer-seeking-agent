1471d6676128dfff4a453a02023cc52519f6504e
Jun Masui <76625904+junmasui@users.noreply.github.com>
2025-06-10 16:31:36 -0400

    new services

---
+++ nemo/docker/build_images.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e  # Exit immediately on error.
+set -u  # Unbound variables are errors.
+set -o pipefail  # Use right-most non-zero exit code from a pipe.
+
+if [ -d nemoguardrails ]
+then
+    rm -rf nemoguardrails
+fi
+
+
+#
+# See: https://docs.nvidia.com/nemo/guardrails/latest/user-guides/advanced/using-docker.html#build-the-docker-images
+#
+
+git clone https://github.com/NVIDIA/NeMo-Guardrails.git nemoguardrails
+
+cd nemoguardrails
+(
+docker build -t nemo-guardrails .
+)
+
+(
+cd nemoguardrails/library/jailbreak_detection
+docker build -t nemo-jailbreak-detection-heuristics .
+docker build -t nemo-jailbreak-detection-heuristics-gpu .
+)
