0adb2646979d142255c92aa4d3ee4d42b4a5158c
Jun Masui <76625904+junmasui@users.noreply.github.com>
2025-08-25 15:58:45 -0400

    workaround for circular bootstrap

--- presidio/run_presidio_analyzer.sh
+++ presidio/run_presidio_analyzer.sh
@@ -1,14 +1,4 @@
-
-# Set environment variables from mounted secrets files
-SECRETS_MOUNT="${SECRETS_MOUNT:-/run/secrets}"
-# shellcheck disable=SC2046
-export $( grep -h -v "^#" "${SECRETS_MOUNT}"/*_secrets | xargs -n1 )
-
-poetry add /dist/core_telemetry_distro-0.1.0-py3-none-any.whl
-poetry add /dist/core_telemetry_instrumentation-0.1.0-py3-none-any.whl
-
-poetry add opentelemetry-instrumentation-wsgi
-poetry add opentelemetry-instrumentation-flask
+#!/usr/bin/env bash
 
 echo "WORKERS $WORKERS"
 echo "PORT $PORT"
