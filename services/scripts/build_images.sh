cc2bd705b0a1d32df9a1a9206025ed495ff8b33c
Jun Masui <76625904+junmasui@users.noreply.github.com>
2025-09-08 14:25:35 -0400

    improve security

--- services/scripts/build_images.sh
+++ services/scripts/build_images.sh
@@ -5,7 +5,23 @@ set -u  # Unbound variables are errors.
 set -o pipefail  # Use right-most non-zero exit code from a pipe.
 
 # Build container images.
-for SUBDIR in slim-util nemo api-server webui-server
+for SUBDIR in slim-util api-server webui-server
+do
+    ( cd "$SUBDIR"/build ; ./build_images.sh )
+done
+
+# Build Python distributions.
+./scripts/build_python_packages.sh
+
+EXIT_CODE="$?"
+if [ "$EXIT_CODE" != 0 ]
+then
+    echo "Error building Python packages"
+    exit -1
+fi
+
+# Build container images.
+for SUBDIR in nemo
 do
     ( cd "$SUBDIR"/build ; ./build_images.sh )
 done
